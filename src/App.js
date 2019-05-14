10add00dfa0dd3a4f9a07fbca5792b03a5521c6c
Admin <admin@Admins-MBP.lan>
2019-05-14 19:43:38 +0100

    oauth working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import queryString from 'query-string'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      // user: '',
+      tracks: []
+    }
+  }
+  render () {
+    const { artist, followers, imageUrl, tracks, user } = this.state
+    console.log(artist, followers, imageUrl, tracks, user)
+    console.log(this.state.user, '<<<<<<<')
+
+    return (
+      <div className='App'>
+        {this.state.user ? (
+          <div>
+            <h1>A Webpage By Christopher Smith</h1>
+          </div>
+        ) : (
+          <button
+            id='signIn'
+            onClick={() => {
+              window.location = window.location.href.includes('localhost')
+                ? 'http://localhost:8888/login'
+                : 'https://route-note-lander-be.herokuapp.com/login'
+            }}
+            style={{ padding: '20px', fontSize: '50px', marginTop: '20px' }}
+          >
+            Login To Spotify
+          </button>
+        )}
+      </div>
+    )
+  }
+  componentDidMount () {
+    let parsed = queryString.parse(window.location.search)
+    let accessToken = parsed.access_token
+    // console.log(accessToken)
+    if (!accessToken) return
+    else {
+      fetch('https://api.spotify.com/v1/me', {
+        headers: { Authorization: 'Bearer ' + accessToken }
+      })
+        .then(response => response.json())
+        .then(data =>
+          this.setState({
+            user: data
+          })
+        )
+    }
+
+    fetch('https://api.spotify.com/v1/artists/0Q5FNNZ8ieJV9q0YR9boTY', {
+      headers: { Authorization: 'Bearer ' + accessToken }
+    })
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          artist: data.name,
+          followers: data.followers.total,
+          imageUrl: data.images[0].url
+        })
+      )
+
+    fetch('https://api.spotify.com/v1/artists/0Q5FNNZ8ieJV9q0YR9boTY/top-tracks?country=GB', {
+      headers: { Authorization: 'Bearer ' + accessToken }
+    })
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          tracks: data.tracks.map(track => {
+            console.log(data.tracks)
+            return {
+              song: track.name,
+              released: track.album.release_date,
+              length: track.duration_ms,
+              rating: track.popularity
+            }
+          })
+        })
+      )
+  }
 }
 
-export default App;
+export default App
